c05489711e56f2c559f030199f8f3f7055303135
Benedikt Schächner <benedikt.schaechner@gmail.com>
2026-02-22 13:01:39 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
+
 import type {NextConfig} from 'next';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -33,4 +38,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
